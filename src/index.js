ef8e27b0f2a933d16d6498f4266acd427b1464e6
pete3249 <katelynrpeterson16@gmail.com>
2020-12-03 09:33:06 -0500

    commit tree 123aac577f53c99af5cfca9596cf84d22a59601c

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 console.log('%c HI', 'color: firebrick')
+
+function fetchImages() {
+    return fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(resp => resp.json())
+    .then(json => renderImages(json))
+}
+
+function renderImages(images) {
+    const imgs = images["message"]
+    const imgContainer = document.querySelector('#dog-breeds')
+    imgs.forEach(image => {
+        const img = document.createElement('img')
+        img.src = image
+        imgContainer.appendChild(img)
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('The Dom has loaded')
+    fetchImages()
+})
